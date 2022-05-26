27cfe14dfdfb41be62919e03c90177705d439393
Alexey Zimin <lexx.jock@gmail.com>
2022-05-26 16:18:43 +1000

    added method

--- src/main/java/ru/radio/domain/Radio.java
+++ src/main/java/ru/radio/domain/Radio.java
@@ -1,4 +1,33 @@
 package ru.radio.domain;
 
 public class Radio {
+    private int currentStation;
+    private int currentVolume;
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation < 0) {
+            return;
+        }
+        if (currentStation > 9) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume < 0) {
+            return;
+        }
+        if (currentVolume > 10) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
 }
--- src/test/java/ru/radioTest/domain/RadioTest.java
+++ src/test/java/ru/radioTest/domain/RadioTest.java
@@ -1,5 +1,20 @@
 package ru.radioTest.domain;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.radio.domain.Radio;
+
 public class RadioTest {
-    
+
+    @Test
+    void findMax () {
+        Radio rad = new Radio();
+        rad.setCurrentStation(5);
+
+        int actual = rad.getCurrentStation();
+        int expected = 5;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
